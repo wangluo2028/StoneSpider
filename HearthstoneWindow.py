6f45eee728fff75f4b85ba71c2f41f0d97c576e5
annsar <wanghc-c@glodon.com>
2019-12-17 20:44:45 +0800

    fix issue of finding window and add test

--- HearthstoneWindow.py
+++ HearthstoneWindow.py
@@ -15,11 +15,18 @@ class HearthstoneWindow:
 
     def autoSearchWindow(self):
         #self._hwndHearthstone = win32gui.FindWindow(None, self.hsLang.windowTitle)
-        self._hwndHearthstone = gw.getWindowsWithTitle(self.hsLang.windowTitle)[0]
+        hearthstoneWindows = gw.getWindowsWithTitle(self.hsLang.windowTitle)
+        self._hwndHearthstone = None
+        for hsWindow in hearthstoneWindows:
+            if hsWindow.title == self.hsLang.windowTitle:
+                self._hwndHearthstone = hsWindow
+                break
 
     def testMaximize(self):
         #windowSize = win32gui.GetWindowRect(self._hwndHearthstone)
+        self._hwndHearthstone.activate()
+        self._hwndHearthstone.moveTo(0, 0)
         print(self._hwndHearthstone.isMaximized)
         print(self._hwndHearthstone.isMinimized)
-        self._hwndHearthstone.maximize()
-        self._hwndHearthstone.minimize()
+        #self._hwndHearthstone.maximize()
+        #self._hwndHearthstone.minimize()
