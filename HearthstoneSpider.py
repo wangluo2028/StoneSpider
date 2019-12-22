e5c90bfd05b288523acb487f24951da9c635cf65
u1095279206742 <1095279206742@tianchi.com>
2019-12-22 21:54:55 +0800

    some other things

--- HearthstoneSpider.py
+++ HearthstoneSpider.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 #pip freeze > requirements.txt
+#git config credential.helper store
+#git push https://github.com/owner/repo.git
+#git config --global credential.helper 'cache --timeout 7200'
+#git remote set-url origin git@github.com:username/repo.git
 
 import HearthstoneLanguage
 import HearthstoneWindow
